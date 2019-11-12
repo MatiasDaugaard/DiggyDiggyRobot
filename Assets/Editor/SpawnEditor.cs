4415532364a4e868dcdc25ba932aee4a8034e4e7
ThomasRopke <10065225+ThomasRopke@users.noreply.github.com>
2019-11-12 21:46:03 +0100

    Spawn button

---
+++ Assets/Editor/SpawnEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Spawn))]
+public class SpawnEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Spawn spawn = (Spawn)target;
+        if (GUILayout.Button("Generate World"))
+        {
+            spawn.GenerateWorld();
+        }
+    }
+}
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -7,8 +7,15 @@ public class Spawn : MonoBehaviour
     [SerializeField]
     private GameObject go;
 
-    void Start()
+    public void GenerateWorld()
     {
+        // Cleanup
+        while (transform.childCount > 0)
+        {
+            GameObject.DestroyImmediate(transform.GetChild(0).gameObject);
+        }
+
+        // Generate blocks
         for (int y = -20; y < 0; y++)
         {
             for (int z = -10; z <= 10; z++)
@@ -16,16 +23,8 @@ public class Spawn : MonoBehaviour
                 GameObject instance = Instantiate(go);
                 Transform t = instance.transform;
                 t.parent = transform;
-                t.position = new Vector3(0.25f, 0.5f*y, z*0.5f);
+                t.position = new Vector3(0.25f, 0.5f * y, z * 0.5f);
             }
-
-
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }

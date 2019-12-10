97a7a58a8d1e3246facb9bc1ef63e4fd0295412c
ThomasRopke <10065225+ThomasRopke@users.noreply.github.com>
2019-12-10 10:46:23 +0100

    Map Generator

---
+++ Assets/Scripts/MineralCluster.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class MineralCluster : ScriptableObject
+{
+    public GameObject Block {
+        get { return block; }
+    }
+
+    public MineralType Type {
+        get { return Block.GetComponent<Mineral>().Type; }
+    }
+
+    public string Name {
+        get { return Type.ToString(); }
+    }
+
+    public int Amount {
+        get { return amount; }
+    }
+
+    public float Probability {
+        get { return probability; }
+    }
+
+    public float Decay {
+        get { return decay; }
+    }
+
+    public int MinimumDepth {
+        get { return minDepth; }
+    }
+
+    public int MaximumDepth {
+        get { return maxDepth; }
+    }
+
+    [SerializeField]
+    private GameObject block;
+    [SerializeField]
+    private int amount;
+    [SerializeField]
+    private float probability;
+    [SerializeField]
+    private float decay;
+    [SerializeField]
+    private int minDepth;
+    [SerializeField]
+    private int maxDepth;
+   
+}
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Spawn : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> blocks;
+    private List<MineralCluster> clusters;
 
     public void GenerateWorld()
     {
@@ -17,17 +17,17 @@ public class Spawn : MonoBehaviour
 
         // Map blocks
         var map = new Dictionary<MineralType, GameObject>();
-        foreach (GameObject blockType in blocks)
+        foreach (MineralCluster cluster in clusters)
         {
-            Mineral m = blockType.GetComponent<Mineral>();
-            map.Add(m.Type, blockType);
-            Debug.Log(m.Type);
+            map.Add(cluster.Type, cluster.Block);
         }
 
         // Generate matrix
-        int width = 21;
-        int height = 20;
-        MineralType[,] world = new MineralType[width,height];
+        int width = 101;
+        int height = 100;
+        MineralType[,] world = new MineralType[width, height];
+
+        // Fill with ground
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
@@ -36,13 +36,19 @@ public class Spawn : MonoBehaviour
             }
         }
 
-        for (int i = 0; i < 10; i++)
+        // Add clusters
+        foreach (MineralCluster cluster in clusters)
         {
-            int x = Random.Range(0, width);
-            int y = Random.Range(0, height/2);
-            world[x, y] = MineralType.Copper;
+            for (int i = 0; i < cluster.Amount; i++)
+            {
+                int x = Random.Range(0, width);
+                int min = height - cluster.MaximumDepth;
+                int max = height - cluster.MinimumDepth;
+                int y = Random.Range(min, max);
+                AddCluster(world, cluster.Type, new Vector2(x, y), cluster.Probability, cluster.Decay, min, max);
+            }
         }
-        
+
         // Generate blocks
         GameObject block;
         for (int y = 0; y < height; y++)
@@ -53,11 +59,40 @@ public class Spawn : MonoBehaviour
                 int blockY = y - height;
                 int blockZ = z - width / 2;
                 map.TryGetValue(blockType, out block);
-                GameObject instance = Instantiate(block);
-                Transform t = instance.transform;
-                t.parent = transform;
-                t.position = new Vector3(0.25f, 0.5f * blockY, blockZ * 0.5f);
+                Vector3 position = new Vector3(0.25f, 0.5f * blockY, blockZ * 0.5f);
+                AddBlock(block, position);
             }
+        }
+    }
+
+    private void AddCluster(MineralType[,] world, MineralType mineral, Vector2 location, float probability, float decay, int min, int max)
+    {
+        int width = world.GetLength(0);
+        int height = world.GetLength(1);
+        int x = (int) location.x;
+        int y = (int) location.y;
+        float rand = Random.Range(0f, 1f);
+        if (x >= 0 && x < width && y >= min && y < max &&
+            world[x, y] == MineralType.Ground && 
+            rand < probability)
+        {
+            world[x, y] = mineral;
+            probability *= decay;
+            AddCluster(world, mineral, location + new Vector2(0, 1), probability, decay, min, max);
+            AddCluster(world, mineral, location - new Vector2(0, 1), probability, decay, min, max);
+            AddCluster(world, mineral, location + new Vector2(1, 0), probability, decay, min, max);
+            AddCluster(world, mineral, location - new Vector2(1, 0), probability, decay, min, max);
+        }
+    }
+
+    private void AddBlock(GameObject block, Vector3 position)
+    {
+        if (block)
+        {
+            GameObject instance = Instantiate(block);
+            Transform t = instance.transform;
+            t.parent = transform;
+            t.position = position;
         }
     }
 }
